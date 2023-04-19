318e14d0e0785f51374389377255d121d2a4d026
程爱民 <chengam@idea-group.cn>
2023-04-19 11:42:04 +0800

    fix: 替换页面标题
    Signed-off-by: 程爱民 <chengam@idea-group.cn>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "爱德数智AI - 智能助手",
+  description: "您的个人聊天机器人。",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "爱德数智AI - 智能助手",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
